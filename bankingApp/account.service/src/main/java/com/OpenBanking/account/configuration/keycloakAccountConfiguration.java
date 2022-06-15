da37cb2585ef3075cf3da9abc071d890e260d067
zineb drissi <drissizineb6@gmail.com>
2022-06-15 18:51:22 +0100

    project SpringBoot

--- bankingApp/account.service/src/main/java/com/OpenBanking/account/configuration/keycloakAccountConfiguration.java
+++ bankingApp/account.service/src/main/java/com/OpenBanking/account/configuration/keycloakAccountConfiguration.java
@@ -8,11 +8,16 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter;
 import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter;
 import org.keycloak.adapters.springsecurity.filter.QueryParamPresenceRequestMatcher;
 import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -20,9 +25,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -78,17 +88,59 @@ public class keycloakAccountConfiguration extends KeycloakWebSecurityConfigurerA
     
        // super.configure(http);
     	 http
-         .csrf().disable().
+    	 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+         .and()
+         .addFilterBefore(keycloakPreAuthActionsFilter(), LogoutFilter.class)
+         .addFilterBefore(keycloakAuthenticationProcessingFilter(), X509AuthenticationFilter.class)
+         .csrf().disable().cors().disable().
          authorizeHttpRequests()
          .antMatchers("/login*").permitAll()
          .antMatchers("/resources/**").permitAll()
          .antMatchers("/*.html").permitAll()
-         //.antMatchers("/accounts/**").hasRole("admin")
+         .antMatchers("/accounts/**").hasRole("admin")
          .anyRequest()
          .authenticated()
          .and()
-         .formLogin();		
+         .formLogin()
+         ;		
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+      web
+          .ignoring()
+          .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+    
+    @Bean
+    public FilterRegistrationBean<KeycloakPreAuthActionsFilter> keycloakPreAuthActionsFilterRegistrationBean(
+        KeycloakPreAuthActionsFilter filter) {
+      FilterRegistrationBean<KeycloakPreAuthActionsFilter> registrationBean = new FilterRegistrationBean<>(
+          filter);
+      registrationBean.setEnabled(false);
+      return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<KeycloakAuthenticatedActionsFilter> keycloakAuthenticatedActionsFilterBean(
+        KeycloakAuthenticatedActionsFilter filter) {
+      FilterRegistrationBean<KeycloakAuthenticatedActionsFilter> registrationBean = new FilterRegistrationBean<>(
+          filter);
+      registrationBean.setEnabled(false);
+      return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<KeycloakSecurityContextRequestFilter> keycloakSecurityContextRequestFilterBean(
+        KeycloakSecurityContextRequestFilter filter) {
+      FilterRegistrationBean<KeycloakSecurityContextRequestFilter> registrationBean = new FilterRegistrationBean<>(
+          filter);
+      registrationBean.setEnabled(false);
+      return registrationBean;
+    }
    
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
